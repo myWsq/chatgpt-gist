153aa972b33cdb7da64aa666186826fe1b693ccc
myWsq <wsq961@outlook.com>
2023-03-11 15:28:14 +0800

    fix: github logo color

--- src/components/LogoGithub.tsx
+++ src/components/LogoGithub.tsx
@@ -1,7 +1,7 @@
 export const LogoGithub: React.FunctionComponent = () => {
   return (
     <svg
-      fill="#FFFFFF"
+      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 30 30"
       width="30"
